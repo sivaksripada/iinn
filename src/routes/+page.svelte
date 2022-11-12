1bcaf2d1646b852a047a4a8c8d6c4b8b0020bd45
sivaksripada <sivaksripada@gmail.com>
2022-11-12 13:38:56 -0500

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="flex flex-row">
+	<p class="font-bold">test</p>
+</div>
