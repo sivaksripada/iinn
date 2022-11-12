659ee28e6aa7b7651a8d13c8dddec0527b92bc32
sivaksripada <sivaksripada@gmail.com>
2022-11-12 14:59:45 -0500

    supabase

---
+++ src/lib/procedureTemplate.js
@@ -0,0 +1,5 @@
+export let template = {
+    data: {
+        test: []
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
 <script>
+	import { template } from '$lib/procedureTemplate';
+
+	import { supabaseClient } from '$lib/db';
+
+	const signInWithGoogle = async () => {
+		const { data, error } = await supabaseClient.auth.signInWithOAuth({
+			provider: 'google'
+		});
+		console.log(data);
+		console.log(error);
+	};
 </script>
 
 <svelte:head>
@@ -7,7 +18,9 @@
 </svelte:head>
 
 <div class="flex flex-row">
-	<p class="font-bold">test</p>
+	<p class="font-bold">{JSON.stringify(template)}</p>
 </div>
 
 <p />
+
+<p on:click={signInWithGoogle}>sign in with google</p>
