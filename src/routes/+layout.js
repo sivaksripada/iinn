6eb641eae38b2262645715239661e33faed394f7
sivaksripada <sivaksripada@gmail.com>
2022-11-13 14:34:27 -0500

    added jumbotron to page.svelte

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,6 @@
-import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+// import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
-export const load = async (event) => {
-  const { session } = await getSupabase(event);
-  return { session };
-};
+// export const load = async (event) => {
+//   const { session } = await getSupabase(event);
+//   return { session };
+// };
